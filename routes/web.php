49ad91a5afb918c4a50886f591f191acbdf227fa
Verrell <deanverrell@gmail.com>
2021-12-04 22:26:52 +0700

    push commit

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('welcome');
+});
 
-Route::get('login', function () {
-    return view('login');
-})->name('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('checkout', function () {
-    return view('checkout');
-})->name('checkout');
-
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
+require __DIR__.'/auth.php';
